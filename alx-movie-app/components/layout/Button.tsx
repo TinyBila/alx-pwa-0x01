c3584bbfbe6a3903e7bee2b753c47e6960a3422b
Tiny <129532205+TinyBila@users.noreply.github.com>
2025-10-16 21:05:47 +0200

    Create Button.tsx

---
+++ alx-movie-app/components/layout/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button: React.FC = () => {
+  return (
+    <button className="px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-700">
+      Layout Button
+    </button>
+  );
+};
+
+export default Button;
